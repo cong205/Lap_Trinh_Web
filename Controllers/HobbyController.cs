b8aba615436f54d208fff9fbf920475080d5c1fb
cong205 <nguyendinhcong20112005@gmail.com>
2025-08-26 21:45:57 +0700

    commit1

---
+++ Controllers/HobbyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace NguyenDinhCong.Controllers
+{
+    public class HobbyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/InfoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace NguyenDinhCong.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Basic()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
